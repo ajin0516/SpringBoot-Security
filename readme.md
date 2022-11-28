ac2d6c10bead22a158f2854e5402c11ec7c1cd36
ajin0516 <dkwls7432@naver.com>
2022-11-28 12:04:55 +0900

    유저 로그인 기능 1. UserController - Post 회원가입 기능

---
+++ readme.md
@@ -0,0 +1,6 @@
+## EndPoints
+
+유저 로그인 기능
+
+회원가입 기능
+[POST] /api/v1/users/join
---
+++ src/main/java/com/hospital/review/domain/Response.java
@@ -0,0 +1,19 @@
+package com.hospital.review.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Response<T> {
+    private String resultCode;
+    private T result; // 성공 반환 시 T로 감싸서 반
+
+    private static Response<Void> error(String resultCode) {
+        return new Response(resultCode, null);
+    }
+
+    public static <T> Response<T> success(T result) {
+        return new Response("SUCCESS", result);
+    }
+}
